20dba8feab9e2757d333605d39e3aae9314106e7
laxman1129 <laxman1129@gmail.com>
2024-04-28 20:17:05 +0400

    changes

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import os
 from src.input import ingest
 from src.output import privateGPT
 from dotenv import load_dotenv  # for loading environment variables
+import shutil
 
 load_dotenv()
 
 source_directory = os.environ.get("SOURCE_DIRECTORY","source_documents")
+persist_directory = os.environ.get("PERSIST_DIRECTORY")
+
+shutil.rmtree(persist_directory)
 
 def main():
     print("------------------------------->Ingesting documents...")
